30851f46cdad9dcd7e3c09139b6c97f42bdba730
aidris823 <pokeanand@gmail.com>
2018-10-19 11:00:04 -0400

    Figuring out how to calloc w/ a 2D array.

--- erastothenes.c
+++ erastothenes.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include <math.h>
 
-const int numVals = 1000000*log(1000000)*1.15; //Total number of values to look at
+const int num_vals =1000000*log(1000000)*1.15; //Total number of values to look at
 int primeList[1000000]; //We want millionth prime number
-int integers[numVals][2];
+int *integers = (int *)malloc(num_vals * 2 * sizeof(int));
+
+
 
 void initialize(){
   for (int i = 2; i < numVals; i++){
@@ -17,7 +19,13 @@ void initialize(){
   }
 }
 
-void calcPrimes(){
+void print_array(int n){
+  for (int i = 0; i < n; i++){
+    printf("Number: %d \n Value: %d",integers[i][0],integers[i][1]);
+  }
+}
+/*
+void calc_primes(){
   int primeNo = 0;
   numVals[0][1] = 1;
   for (int i = 2; i < numVals; i++){
@@ -27,13 +35,16 @@ void calcPrimes(){
   }
   for (int i = 2; i < numVals; i++){
     if (integers[i][1] != 2){
+      integers[i][1] = 1;
       primeList[primeNo] = integers[i][0];
       primeNo++;
     }
   }
 }
-
+*/
 int main(){
-  
+  //Testing Initialize
+  initialize();
+  print_array(20);
   return 0;
 }
